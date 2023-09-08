e5e6b4985b278cf7542d24168a9ca705f5a58549
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-09-08 12:39:46 -0500

    scrolling issues, button matching.

--- js/app.js
+++ js/app.js
@@ -13,8 +13,9 @@ document.getElementById("goHome").addEventListener("click", function (e) {
 window.addEventListener("scroll", function () {
   var navbar = document.getElementById("navbar");
   if (
-    window.scrollY > 0 &&
-    window.innerHeight + window.scrollY + 1 < document.body.offsetHeight
+    window.scrollY > 0
+    // &&
+    // window.innerHeight + window.scrollY + 1 < document.body.offsetHeight
   ) {
     navbar.classList.remove("navbar-initial");
     navbar.classList.add("navbar-active");
